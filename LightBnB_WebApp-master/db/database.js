2c5b770a983c0f477693e0ebc730a82882a5a4c2
Paul Formby <paulformby@icloud.com>
2023-12-11 21:50:56 +0000

    Update functions to use the lightbnb database with SQL queries

--- LightBnB_WebApp-master/db/database.js
+++ LightBnB_WebApp-master/db/database.js
@@ -1,5 +1,19 @@
 const properties = require("./json/properties.json");
 const users = require("./json/users.json");
+const { Pool } = require("pg");
+
+const pool = new Pool({
+  user: "vagrant",
+  password: "123",
+  host: "localhost",
+  database: "lightbnb",
+  port: 5432,
+});
+
+pool
+  .connect()
+  .then(() => console.log("Connected to the database"))
+  .catch((err) => console.error("Error connecting to the database", err));
 
 /// Users
 
@@ -8,15 +22,22 @@ const users = require("./json/users.json");
  * @param {String} email The email of the user.
  * @return {Promise<{}>} A promise to the user.
  */
+
+// Function to retrieve a user based on their email using the LightBnB database
 const getUserWithEmail = function (email) {
-  let resolvedUser = null;
-  for (const userId in users) {
-    const user = users[userId];
-    if (user && user.email.toLowerCase() === email.toLowerCase()) {
-      resolvedUser = user;
-    }
-  }
-  return Promise.resolve(resolvedUser);
+  // Using the pool to execute a SQL query to select a user based on the provided email
+  return pool
+    .query("SELECT * FROM users WHERE LOWER(email) = LOWER($1) LIMIT 1", [
+      email,
+    ])
+    .then((result) => {
+      // Assuming email is unique, so we only expect one result
+      return result.rows[0];
+    })
+    .catch((err) => {
+      // Handling any errors that might occur during the database query
+      console.error(err.message);
+    });
 };
 
 /**
@@ -24,8 +45,19 @@ const getUserWithEmail = function (email) {
  * @param {string} id The id of the user.
  * @return {Promise<{}>} A promise to the user.
  */
+
+// Function to retrieve a user based on their ID using the LightBnB database
 const getUserWithId = function (id) {
-  return Promise.resolve(users[id]);
+  return pool
+    .query("SELECT * FROM users WHERE id = $1 LIMIT 1", [id])
+    .then((result) => {
+      // Assuming user IDs are unique, so we only expect one result
+      return result.rows[0];
+    })
+    .catch((err) => {
+      // Handling any errors that might occur during the database query
+      console.error(err.message);
+    });
 };
 
 /**
@@ -33,11 +65,26 @@ const getUserWithId = function (id) {
  * @param {{name: string, password: string, email: string}} user
  * @return {Promise<{}>} A promise to the user.
  */
+
+// Function to add a new user to the LightBnB database
 const addUser = function (user) {
-  const userId = Object.keys(users).length + 1;
-  user.id = userId;
-  users[userId] = user;
-  return Promise.resolve(user);
+  // Destructuring user object properties
+  const { name, email, password } = user;
+
+  // Using the pool to execute a SQL query to insert a new user into the 'users' table
+  return pool
+    .query(
+      "INSERT INTO users (name, email, password) VALUES ($1, $2, $3) RETURNING *",
+      [name, email, password]
+    )
+    .then((result) => {
+      // Returning the newly inserted user object, including the auto-generated ID
+      return result.rows[0];
+    })
+    .catch((err) => {
+      // Handling any errors that might occur during the database query
+      console.error(err.message);
+    });
 };
 
 /// Reservations
@@ -59,12 +106,16 @@ const getAllReservations = function (guest_id, limit = 10) {
  * @param {*} limit The number of results to return.
  * @return {Promise<[{}]>}  A promise to the properties.
  */
-const getAllProperties = function (options, limit = 10) {
-  const limitedProperties = {};
-  for (let i = 1; i <= limit; i++) {
-    limitedProperties[i] = properties[i];
-  }
-  return Promise.resolve(limitedProperties);
+
+const getAllProperties = (options, limit = 10) => {
+  return pool
+    .query(`SELECT * FROM properties LIMIT $1`, [limit])
+    .then((result) => {
+      return result.rows;
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
 };
 
 /**
