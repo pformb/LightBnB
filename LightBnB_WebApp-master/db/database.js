3dcbcd0fa9507a3a037c907bf820b73170b97102
Paul Formby <paulformby@icloud.com>
2023-12-13 00:46:21 +0000

    clean up comments

--- LightBnB_WebApp-master/db/database.js
+++ LightBnB_WebApp-master/db/database.js
@@ -226,10 +226,10 @@ const addProperty = function (property) {
   // Using the pool to execute a SQL query to insert a new property into the 'property' table
   return pool
     .query(
-      `INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url,
-        cost_per_night, street, city, province, post_code, country, parking_spaces,
-          number_of_bathrooms, number_of_bedrooms) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, 
-            $9, $10, $11, $12, $13, $14) RETURNING *`,
+    `INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, 
+     cover_photo_url, cost_per_night, street, city, province, post_code, country,
+     parking_spaces, number_of_bathrooms, number_of_bedrooms) VALUES ($1, $2, $3, 
+     $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14) RETURNING *`,
       [
         owner_id,
         title,
